df386fd984c25f879ea148c4eba0421add169442
Chen Xie <chenxie2016@outlook.com>
2016-05-11 03:24:16 -0700

    0511

--- shell/scale/modules/ssh_slave_auto.sh
+++ shell/scale/modules/ssh_slave_auto.sh
@@ -5,12 +5,13 @@ then
 	echo "1st arg should hadoop_user"
 fi
 
+#for root
 # change /etc/hosts
 chown $1:$1 /etc/hosts
 # change authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
-echo "setup hadoop_user"
 ##copy to hadoop_user
+echo "setup hadoop_user"
 cp -r ~/.ssh /home/$1/
 chown -R $1:$1 /home/$1/.ssh
--- shell/start/setup_master.sh
+++ shell/start/setup_master.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+java_file=/home/`whoami`/jdk.tar.gz
+hadoop_file=/home/`whoami`/hadoop.tar.gz
+
+#ssh
 ssh-keygen -t rsa
 
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
@@ -14,3 +18,19 @@ echo "master ip:"
 read masterIp
 
 echo "$masterIp master" >> /etc/hosts
+
+#JAVA
+if [ -z $JAVA_HOME ];
+then
+	tar -zxf $java_file -C /home/`whoami`/
+	echo "export JAVA_HOME=/home/`whoami`/jdk1.8.0_92" >> ~/.bashrc
+	echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
+fi
+
+#HADOOP
+if [ -z $HADOOP_HOME ];
+then
+	tar -zxf $hadoop_file -C /home/`whoami`/
+	echo "export HADOOP_HOME=/home/`whoami`/hadoop" >> ~/.bashrc
+	echo 'export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin' >> ~/.bashrc
+fi
