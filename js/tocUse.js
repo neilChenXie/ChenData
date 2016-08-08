6206a808946b60e8a30af46b6974fc8018bb3a35
Chen Xie <xiec@usc.edu>
2016-08-08 22:47:18 +0800

    add content

--- js/tocUse.js
+++ js/tocUse.js
@@ -1,10 +1,8 @@
 var config = {
 	headers: 'h3, h4, h5, h6',
-	minimumHeaders: 2,
-	classes: { list: 'lorem ipsum',
-		item: 'dolor sit amet'
-	}
-}
+	minimumHeaders: 1,
+};
+
 $(document).ready(function() {
 	$('#toc').toc(config);
 });
